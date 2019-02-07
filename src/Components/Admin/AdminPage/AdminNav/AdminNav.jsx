f9f0ed977d00350076bf19d15a1f9d0116b3a318
AxYleN <jeka2644@gmail.com>
2019-02-07 17:32:57 +0300

    Добавлена навигация в сайдбар админ-панели

---
+++ src/Components/Admin/AdminPage/AdminNav/AdminNav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './AdminNav.css';
+
+export default function AdminNav(props) {
+  const matchUrl = props.matchUrl;
+  const linkStyles = {
+    className: 'admin-nav__link',
+    activeClassName: 'admin-nav__link--active',
+  };
+
+  return (
+    <nav className="admin-nav">
+      <NavLink {...linkStyles} to={matchUrl + '/orders'}>
+        Список заказов
+      </NavLink>
+      <NavLink {...linkStyles} to={matchUrl + '/price'}>
+        Товары
+      </NavLink>
+    </nav>
+  );
+}
--- src/Components/Admin/AdminPage/AdminPage.jsx
+++ src/Components/Admin/AdminPage/AdminPage.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './AdminPage.css';
 
+import AdminNav from './AdminNav/AdminNav';
+
 export default function AdminPage(props) {
   const matchUrl = props.match.url;
 
   return (
     <div className="admin__container">
-      <aside className="admin__sidebar">Сайдбар</aside>
+      <aside className="admin__sidebar">
+        <AdminNav matchUrl={matchUrl}/>
+      </aside>
       <main className="admin__main-container">
         <Switch>
           <Route path={matchUrl + '/price'} component={() => 'Цены'} />
