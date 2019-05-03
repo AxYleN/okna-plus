b7e71d94f73576f07443fd6f727d03bcd21bdf67
AxYleN <jeka2644@gmail.com>
2019-05-04 00:53:34 +0300

    При добавлении товара в корзину кнопка изменяется

--- src/Components/ConfigurationPage/ConfigurationPage.jsx
+++ src/Components/ConfigurationPage/ConfigurationPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PageLayout from '../PageLayout/PageLayout';
 import ProductEdit from './../ProductEdit/ProductEdit';
 import axios from 'axios';
@@ -7,6 +7,7 @@ import { getOpenToValues, getDefaultParams } from 'lib';
 export default function ConfigurationPage(props) {
   const [product, setProduct] = useState(null);
   const [params, setParams] = useState(null);
+  const [wasAdded, setWasAdded] = useState(false);
 
   useEffect(() => {
     axios
@@ -29,14 +30,25 @@ export default function ConfigurationPage(props) {
       });
   }, []);
 
+  const handleSetParams = useCallback(
+    newParams => {
+      setParams(newParams);
+      if (wasAdded) setWasAdded(false);
+    },
+    [wasAdded],
+  );
+  const handleAdd = useCallback(() => {
+    props.addToCart({ params, count: 1, key: product.product_key });
+    setWasAdded(true);
+  }, [params]);
+
   if (!product || !params) return <PageLayout>Загрузка...</PageLayout>;
+  const btnClasses = wasAdded ? 'btn btn--success' : 'btn';
   return (
     <PageLayout>
-      <ProductEdit params={params} product={product} setParams={setParams}>
-        <button
-          className="btn"
-          onClick={() => props.addToCart({ params, count: 1, key: product.product_key })}>
-          Добавить в корзину
+      <ProductEdit params={params} product={product} setParams={handleSetParams}>
+        <button className={btnClasses} onClick={handleAdd}>
+          {wasAdded ? 'Добавлено' : 'Добавить в корзину'}
         </button>
       </ProductEdit>
     </PageLayout>
